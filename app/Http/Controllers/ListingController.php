5cbaab7e000129fabd77d91c9f1c0e85ecba2d35
Ganang Setyo Hadi <ganang@mhs.usk.ac.id>
2024-05-09 22:27:39 +0700

    adding search bar and its fungcionality

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class ListingController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $listings = Listing::where('is_active', true)
             ->with('tags')
@@ -15,6 +20,31 @@ class ListingController extends Controller
             ->get();
 
         $tags = Tag::orderBy('name')->get();
+
+        if ($request->has('search-bar')) {
+            $query = strtolower($request->get('search-bar'));
+            $listings = $listings->filter(function ($listing) use ($query) {
+                if (Str::contains(strtolower($listing->title), $query)) {
+                    return true;
+                }
+
+                if (Str::contains(strtolower($listing->company), $query)) {
+                    return true;
+                }
+
+                if (Str::contains(strtolower($listing->location), $query)) {
+                    return true;
+                }
+
+                return false;
+            });
+        }
+        if ($request->has('tag')) {
+            $tag = $request->get('tag');
+            $listings = $listings->filter(function ($listing) use ($tag) {
+                return $listing->tags->contains('slug', $tag);
+            });
+        }
 
         return view('listings.index', compact('listings', 'tags'));
     }
