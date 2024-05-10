d17b831c176bedfeee6c8c70a712195b9c62b345
frrhhnn <f_khair@mhs.usk.ac.id>
2024-05-10 16:32:22 +0700

    create lising page

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,20 @@ use App\Http\Controllers;
 Route::get('/', [Controllers\ListingController::class, 'index'])
     ->name('listings.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+    
+    Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
+
+Route::get('/{listing}', [Controllers\ListingController::class, 'show'])
+    ->name('listings.show');
+
+Route::get('/{listing}/apply', [Controllers\ListingController::class, 'apply'])
+    ->name('listings.apply');
